6029ce72173bb4378033e6b902667bc9b1e486c8
Josu Igoa <josuigoa@gmail.com>
2021-05-13 10:43:55 +0200

    async execution

--- Command.hx
+++ Command.hx
@@ -1,21 +1,29 @@
 package;
 
+import js.node.ChildProcess;
+
 using api.IdeckiaApi;
+
 typedef Props = {
-    @:editable("Path to the command")
-    var cmd:String;
-    @:editable("Arguments for the command")
-    var ?args:Array<String>;
+	@:editable("Path to the command")
+	var cmd:String;
+	@:editable("Arguments for the command")
+	var ?args:Array<String>;
 }
 
 class Command extends IdeckiaAction {
-
 	public function execute(currentState:ItemState):ItemState {
-        var status = Sys.command(props.cmd, props.args);
-        if (status != 0) {
-            var args = (props.args == null) ? '' : props.args.join(' ');
-            server.log.error('Something went wrong (status=$status) executing [${props.cmd} ${args}]');
-        }
-        return currentState;
+		var cmd = ChildProcess.spawn(props.cmd, props.args, {shell: true});
+
+		cmd.stdout.on('data', function(data) {
+			server.log.debug('stdout: ' + data);
+		});
+
+		cmd.on('error', function(code) {
+			var args = (props.args == null) ? '' : props.args.join(' ');
+			server.log.error('Something went wrong (code=$code) executing [${props.cmd} ${args}]');
+		});
+
+		return currentState;
 	}
 }
